7c97e5c4c7c1720a7004943d5515906a6d9c08f1
aaradhyasingh2013 <kuki81kumari@gmail.com>
2024-10-20 17:40:25 +0530

    ob

---
+++ main.py
@@ -0,0 +1,4 @@
+class student:
+    grade= 6
+    print("hello i am a student of grade ", grade)
+ob= student()
---
+++ ob.py
@@ -0,0 +1,5 @@
+class vehicle:
+    def __init__(self,max_speed):
+        self.max_speed= max_speed
+x= vehicle(195)
+print(x.max_speed)
